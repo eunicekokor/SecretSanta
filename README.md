0a1a8d650579f12a1a28e3fa0720687b416b4b81
Eunice Emefa Kokor <eek2138@columbia.edu>
2015-12-01 17:21:24 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Secret Santa
+### A simple python script to gen Secret Santa Assignments for your group
+
+## To Setup
+You need any `.json` file with the fields in `sample_info.json` filled out.
+These fields can be changed but need to be reflected in the final `print_to_file`
+function.
+
+## To Run
+`./secretsanta.py yourjsonfile.json`
+It will print out paragraphs you can copy paste in an email.
+
+## Potential Enhancements
+New File with paragraphs or possible email integration
---
+++ secretsanta.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+
+import json
+import random
+import sys
+
+ss_names = None
+
+
+def secret_santa(filename):
+    print filename
+    names = get_ss_names(filename)
+    options = len(names)  # Number of SS players
+    list_remaining = range(options)  # Creates a list from [0, # of SS players)
+
+    for i, n in enumerate(names):
+        # Pick a random player
+        assigned_num = random.choice(list_remaining)
+        while (assigned_num == i):  # Make sure you don't assign to yourself
+            assigned_num = random.choice(list_remaining)  # Pick new person
+        n["assigned_to"] = names[assigned_num]["name"]  # Assign that person
+        list_remaining.remove(assigned_num)  # Remove that so not picked again
+
+    print_to_file(names)
+
+
+def get_ss_names(filename):
+    global ss_names
+    if not ss_names:
+        with open(filename) as f:
+            ss_names = json.loads(f.read())
+
+    return ss_names
+
+
+def print_to_file(list):
+    for person in list:
+        email_string = ("Congrats {}! You have been assigned to {}."
+                        " Their favorite color is {}. These are a few of their"
+                        "favorite things: {}. Have fun being their Secret"
+                        "Santa!").format(person["assigned_to"],
+                                         person["name"],
+                                         person["color"],
+                                         person["likes"])
+        print email_string + "\n"
+if __name__ == "__main__":
+  secret_santa(sys.argv[1])
